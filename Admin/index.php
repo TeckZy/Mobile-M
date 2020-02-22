ccbfb588a6ca24003b3b2cc99c3de8eddef442ea
Akash Singh [h3ck4rz] <akashkumarsngh9@gmail.com>
2020-02-22 17:15:24 +0530

    £9-hom-design: Added a Home Template

--- Admin/index.php
+++ Admin/index.php
@@ -1,5 +1,7 @@
-﻿<!DOCTYPE html>
+﻿F
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,10 +24,11 @@
     <section id="wrapper" class="login-register">
         <div class="login-box login-sidebar">
             <div class="white-box">
-                <form class="form-horizontal form-material" id="loginform" action="manage/manage.adminlogin.php" method="post">
+                <form class="form-horizontal form-material" id="loginform" action="manage/manage.adminlogin.php"
+                    method="post">
                     <a href="javascript:void(0)" class="text-center db">
                         <img src="../images/Mobile%20M%20logo.png" alt="Home" class="img-responsive" />
-                    </a>             
+                    </a>
                     <div class="form-group m-t-40">
                         <div class="col-xs-12">
                             <input class="form-control" name="email" type="email" required="" placeholder="Username">
@@ -33,22 +36,24 @@
                     </div>
                     <div class="form-group">
                         <div class="col-xs-12">
-                            <input class="form-control" name="password" type="password" required="" placeholder="Password">
+                            <input class="form-control" name="password" type="password" required=""
+                                placeholder="Password">
                         </div>
                     </div>
                     <div class="form-group text-center m-t-20">
                         <div class="col-xs-12">
-                            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button>
+                            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light"
+                                type="submit">Log In</button>
                         </div>
                     </div>
-                
+
                 </form>
             </div>
         </div>
     </section>
-    
-    
-    
+
+
+
     <script src="plugins/bower_components/jquery/dist/jquery.min.js"></script>
     <script src="webadmin/bootstrap/dist/js/tether.min.js"></script>
     <script src="webadmin/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -60,7 +65,7 @@
     <script src="plugins/bower_components/jquery-sparkline/jquery.charts-sparkline.js"></script>
     <script src="webadmin/js/custom.min.js"></script>
     <script src="plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
-    
+
 </body>
 
 </html>
